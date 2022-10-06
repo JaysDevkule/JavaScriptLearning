8be0ba008ebd4ccb578048f6ea18e3bd5d4002bd
JaysDevkule <108869129+JaysDevkule@users.noreply.github.com>
2022-10-06 13:52:52 +0530

    created assignment 1 file
    completed task

---
+++ Assignment1.js
@@ -0,0 +1,6 @@
+console.log("Jayshree Kamble");
+console.log("1.Reading 2.Painting 3.Gardening");
+console.info("1.Communication 2.Decision Making 3.Confidence");
+console.warn("1.overthinking 2.lack of patience 3.Insecure");
+console.log(30+50);
+console.log(49*479);
