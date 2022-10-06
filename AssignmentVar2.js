9188b4033baa73cbfaf61b06b4f6d1d83ee54f0e
JaysDevkule <108869129+JaysDevkule@users.noreply.github.com>
2022-10-06 15:08:39 +0530

    updated assignmentvar2 and demo file
    updated assignmentvar2 and demo file

---
+++ AssignmentVar2.js
@@ -0,0 +1,17 @@
+console.log("*********************1.1-Assignment***********************");
+var myFullName;
+console.log(myFullName);
+
+myFullName="Jayshree Kamble";
+console.log(myFullName);
+
+myFullName="Jayshree Vish Kamble";
+console.log(myFullName);
+
+console.log("*********************1.2-Assignment***********************");
+var myLove="I Love only JS";
+console.log(myLove);
+
+console.log("*********************1.3-Assignment***********************"); 
+var firstname="jayshree",lastname="Kamble",age="24",address="Pune",pin_code="411014",gender="Female",is_married="Yes";
+console.log(firstname,lastname,age,address,pin_code,gender,is_married);
--- demo.js
+++ demo.js
@@ -1,1 +1,8 @@
 console.log("JavaScript");
+
+var myapple="straw";
+console.log(myapple);
+
+var myname="mohit sharma";
+myname="mohit raju sharma";
+console.log(myname);
