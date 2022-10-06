c4856433a8e8bbc6a68cc5badc158b3ea829e332
JaysDevkule <108869129+JaysDevkule@users.noreply.github.com>
2022-10-06 15:25:32 +0530

    created assignment 3file
    created assignment 3file and completed given task

---
+++ AssignmentSwap3.js
@@ -0,0 +1,22 @@
+console.log("***********************2.1-Assignment***********************");
+var sweety="Sweety";
+var cutie="Cutie";
+console.log("Before Swapping");
+console.log(sweety,cutie);
+var temp;
+temp=sweety;
+sweety=cutie;
+cutie=temp;
+console.log("After Swapping");
+console.log(sweety,cutie);
+
+console.log("***********************2.2-Assignment***********************");
+var num1="100",num2="200",num3="300",temp1;
+console.log("Before Swapping");
+console.log(num1,num2,num3);
+temp1=num2;
+num2=num1;
+num1=num3;
+num3=temp1
+console.log("After Swapping");
+console.log(num1,num2,num3);
