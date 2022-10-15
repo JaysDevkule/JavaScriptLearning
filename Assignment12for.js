f8e2094693bfa3942ed354b01cca07cccdb8e555
JaysDevkule <108869129+JaysDevkule@users.noreply.github.com>
2022-10-15 14:52:12 +0530

    created assignment 12
    completed assignment 12

---
+++ Assignment12for.js
@@ -0,0 +1,63 @@
+console.log("-------------Assignment 12.1----------------->>>>>>");
+var i=5;
+while (i<=15) {
+    console.log(`${i}`);
+    i++;
+    
+}
+console.log("-------------Assignment 12.2----------------->>>>>>");
+var j=50;
+while (j>=40) {
+    console.log(`${j}`);
+    j--;
+    
+}
+console.log("-------------Assignment 12.3----------------->>>>>>");
+var odd=1;
+console.log("Odd numbers");
+while(odd<=15) 
+{
+    if(odd%2!=0)
+    {
+    console.log(`${odd}`); 
+    }
+    odd++;
+}
+console.log("-------------Assignment 12.4----------------->>>>>>");
+var even=1;
+console.log("Even Numbers");
+while(even<=10) 
+{
+    if(even%2==0)
+    {
+    console.log(`${even}`); 
+    }
+    even++;
+}
+console.log("-------------Assignment 12.5----------------->>>>>>");
+var table=5;
+var k=1;
+console.log("Table of 5");
+while(k<=10)
+{
+console.log(k*table);
+k++;
+}
+console.log("-------------Assignment 12.6----------------->>>>>>");
+var TableOf10=10;
+var l=1;
+console.log("Table of 10");
+while(l<=10)
+{
+console.log(l*TableOf10);
+l++;
+}
+console.log("-------------Assignment 12.7----------------->>>>>>");
+var len=10;
+var m=10;
+console.log("Table of 10 in reverse");
+while(m>=1)
+{
+console.log(m*10);
+m--;
+}
