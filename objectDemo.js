86b4cc5eade828ebba5757df8cb239819d4e2ebd
JaysDevkule <108869129+JaysDevkule@users.noreply.github.com>
2022-11-03 20:58:44 +0530

    updated object demo
    updated object demo

--- objectDemo.js
+++ objectDemo.js
@@ -5,25 +5,35 @@
 //             };
 // console.log(person.name);
 // person.greet();
-class Person{
-    name;
-    age;
-    constructor(name,age)
-    {
-        this.name=name;
-        this.age=age;
-    }
-    play(){
-        console.log("playing cricket");
-    }
-    eat()
-    {
-        console.log("eating mango");
-    }
+// class Person{
+//     name;
+//     age;
+//     constructor(name,age)
+//     {
+//         this.name=name;
+//         this.age=age;
+//     }
+//     play(){
+//         console.log("playing cricket");
+//     }
+//     eat()
+//     {
+//         console.log("eating mango");
+//     }
 
-}
-let anilPerson= new Person("anil",25);
-let ritaPerson= new Person("rita",30);
-anilPerson.name;
-anilPerson.play();
-ritaPerson.eat();
+// }
+// let anilPerson= new Person("anil",25);
+// let ritaPerson= new Person("rita",30);
+// anilPerson.name;
+// anilPerson.play();
+// ritaPerson.eat();
+let date=new Date();
+console.log(date);
+console.log(date.getDate());
+console.log(date.toDateString());
+console.log(date.toTimeString());
+
+console.log(date.getDay());
+console.log(date.getFullYear());
+console.log(date.getHours());
+console.log(date.getMonth());
