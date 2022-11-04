d5e4c80737da9924a4bce377b763151c784ec798
JaysDevkule <108869129+JaysDevkule@users.noreply.github.com>
2022-11-05 00:26:54 +0530

    updated file
    updated file

--- objectDemo.js
+++ objectDemo.js
@@ -27,13 +27,20 @@
 // anilPerson.name;
 // anilPerson.play();
 // ritaPerson.eat();
-let date=new Date();
-console.log(date);
-console.log(date.getDate());
-console.log(date.toDateString());
-console.log(date.toTimeString());
+// let date=new Date();
+// console.log(date);
+// console.log(date.getDate());
+// console.log(date.toDateString());
+// console.log(date.toTimeString());
 
-console.log(date.getDay());
-console.log(date.getFullYear());
-console.log(date.getHours());
-console.log(date.getMonth());
+// console.log(date.getDay());
+// console.log(date.getFullYear());
+// console.log(date.getHours());
+// console.log(date.getMonth());
+const letter=new Set(["a","b","c"]);
+console.log(letter.size);
+let text = "";
+letter.forEach (function(value) {
+  text += value ;
+})
+console.log(text);
